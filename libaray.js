0d4cd6425d006bee4cf6001f97fd910267ef0832
wafanakha <wafanakha15@gmail.com>
2023-12-08 23:41:37 +0700

    adding animation to button

--- libaray.js
+++ libaray.js
@@ -1,10 +1,18 @@
 const library = [];
 
-function Book(title, author, year, pages) {
+function Book(title, author, year, pages, read) {
   this.title = title;
   this.author = author;
   this.year = year;
   this.pages = pages;
+  this.read = read;
 }
 
 function addBook(params) {}
+
+const dialog = document.querySelector("dialog");
+const showBtn = document.querySelector("#show");
+const closeBtn = document.querySelector("#close");
+
+showBtn.addEventListener("click", () => dialog.showModal());
+closeBtn.addEventListener("click", () => dialog.close());
