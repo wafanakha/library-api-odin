15f1120fc280d8690d4788c378d5ee8523c3b93b
wafanakha <wafanakha15@gmail.com>
2023-12-10 21:30:30 +0700

    adding library node on addBook function

--- libaray.js
+++ libaray.js
@@ -11,13 +11,14 @@ function Book(title, author, year, pages, read) {
 function addBook(book) {
   document.querySelector(".library h1").remove();
   document.querySelector(".library h2").remove();
-
+  const library = document.querySelector(".library");
   const bookCards = document.createElement("div");
   const author = document.createTextNode(book.elements["author"].value);
   const title = document.createTextNode(book.elements["title"].value);
   const year = document.createTextNode(book.elements["year"].value);
   const pages = document.createTextNode(book.elements["pages"].value);
   bookCards.appendChild(author, title, year, pages);
+  library.appendChild(bookCards);
 }
 
 const dialog = document.querySelector("dialog");
