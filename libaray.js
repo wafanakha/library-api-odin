d33e65b34d76724fd9a71c113cdfeb3188f204ea
wafanakha <wafanakha15@gmail.com>
2023-12-10 21:38:44 +0700

    convert form response to html element on addBook function

--- libaray.js
+++ libaray.js
@@ -8,23 +8,27 @@ function Book(title, author, year, pages, read) {
   this.read = read;
 }
 
+const dialog = document.querySelector("dialog");
+const showBtn = document.querySelector("#show");
+const closeBtn = document.querySelector("#close");
+
 function addBook(book) {
   console.log(book);
   document.querySelector(".library h1").remove();
   document.querySelector(".library h2").remove();
+
   const library = document.querySelector(".library");
   const bookCards = document.createElement("div");
   const author = document.createTextNode(book.elements["author"].value);
   const title = document.createTextNode(book.elements["title"].value);
   const year = document.createTextNode(book.elements["year"].value);
   const pages = document.createTextNode(book.elements["pages"].value);
-  bookCards.appendChild(author, title, year, pages);
+  bookCards.appendChild(author);
+  bookCards.appendChild(title);
+  bookCards.appendChild(year);
+  bookCards.appendChild(pages);
   library.appendChild(bookCards);
 }
-
-const dialog = document.querySelector("dialog");
-const showBtn = document.querySelector("#show");
-const closeBtn = document.querySelector("#close");
 
 showBtn.addEventListener("click", () => dialog.showModal());
 closeBtn.addEventListener("click", () => dialog.close());
