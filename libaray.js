3a63309ac8b064c03c91316efb8685bdc4a98b90
wafanakha <wafanakha15@gmail.com>
2023-12-10 20:20:59 +0700

    adding addbook (creating book cards node)

--- libaray.js
+++ libaray.js
@@ -8,7 +8,17 @@ function Book(title, author, year, pages, read) {
   this.read = read;
 }
 
-function addBook(book) {}
+function addBook(book) {
+  document.querySelector(".library h1").remove();
+  document.querySelector(".library h2").remove();
+
+  const bookCards = document.createElement("div");
+  const author = document.createTextNode(book.elements["author"].value);
+  const title = document.createTextNode(book.elements["title"].value);
+  const year = document.createTextNode(book.elements["year"].value);
+  const pages = document.createTextNode(book.elements["pages"].value);
+  bookCards.appendChild(author, title, year, pages);
+}
 
 const dialog = document.querySelector("dialog");
 const showBtn = document.querySelector("#show");
